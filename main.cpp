94f0f72f378fd2abc841f5b3373c41564777bc0a
wayu <i@gmail.com>
2023-02-19 14:01:13 +0700

    mainn

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 #include"Major_Electives.cpp"
 #include"Major_Normal_Plan.cpp"
 #include"Required_Courses&Elective.cpp"
-
+#include"StudyMap.cpp"
 using namespace std;
 
-string studyMap(){
-    return "Map";
+void studyMap(){
+    StudyMap();
 }
 
 string search(){
@@ -20,16 +20,10 @@ string plan(){
 }
 
 int main(){
-    // test case
-    CoreCourses("206161");
-    ActiveCitizen("140104");
-    MajorElec("261404");
-    Technology_for_Application("261405");
-    Technology_and_Software_Methods("261102");
-    Basic_Structure_of_the_Systems("261103");
-    Hardware_and_Computer_Architecture("261210");
-    Learner_Required("001101");
-    Innovative_Required("259192");
-    Learner_Elective("201100");
+    cout<<"\t\t\t\t\t\tCurriculum Management\n";
+    studyMap();
+    
+
+   
     return 0;
 }
