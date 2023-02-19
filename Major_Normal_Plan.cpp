cd906df000a14f602b7b218a5998a52f06a3c737
wayu <i@gmail.com>
2023-02-19 12:09:46 +0700

    credit

--- Major_Normal_Plan.cpp
+++ Major_Normal_Plan.cpp
@@ -2,58 +2,21 @@
 #include<string>
 using namespace std;
 
-void Technology_for_Application(string x){
+string Major(string x){
     //There are 21 subjects in total.
     
     // There are 3 subjects in Technology_for_Application
-    string Technology_for_Application_key[] = {"261405","261491","261492"};
+    string Major_key[] = {"261405","261491","261492","261102","261200","261217","261218","261361","261103","261207","261332","261335","261336","261342","261343","261210","261212","261214","261215","261304","261305"};
     string AdvancedComENGR = "Advanced Computer Engineering Technology";
     string ProjSurvey = "Project Survey";
     string Project = "Project";
-    string Technology_for_Application_subject[] = {"AdvancedComENGR","ProjSurvey","Project"};
-    int credit[]={3,1,3};
-    int n = sizeof(Technology_for_Application_key)/sizeof(Technology_for_Application_key[0]);
-        int count = -1;
-            for(int i = 0; i<n;i++){
-                if(x == Technology_for_Application_key[i]){
-                    count = i;
-                    break;
-                }
-            }
 
-            if(count == -1) return 0;
-            else{
-                return Technology_for_Application_key[count]+" "+Technology_for_Application_subject[count];
-        }  
-}
-void Technology_and_Software_Methods(string x){   
-    //There are 5 subjects in Technology_and_Software_Methods
-    string Technology_and_Software_Methods_key[] = {"261102","261200","261217","261218","261361"};
     string ComProgramming = "Computer Programming";
     string ObjectProgramming = "Object-Oriented Programming";
     string DataStructres = "Data Structures for Computer Engineers";
     string Algorithms = "Algorithms for Computer Engineers";
     string SoftwareENGR = "Software Engineering";
-    string Technology_for_Application_subject[] = {"ComProgramming","ObjectProgramming","DataStructres","Algorithms","SoftwareENGR"};
-    int credit[]={3,3,3,3,3};
-    int n = sizeof(Technology_and_Software_Methods_key)/sizeof(Technology_and_Software_Methods_key[0]);
-    int count = -1;
-        for(int i = 0; i<n;i++){
-            if(x == Technology_and_Software_Methods_key[i]){
-               count = i;
-                break;
-            }
-        }
 
-        if(count == -1) return 0;
-        else{
-            return Technology_and_Software_Methods_key[count]+" "+Technology_and_Software_Methods_subject[count];
-        }  
-
-}
-void Basic_Structure_of_the_Systems(string x){
-    //There are 7 subjects in Basic_Structure_of_the_Systems
-    string Basic_Structure_of_the_Systems_key[] = {"261103","261207","261332","261335","261336","261342","261343"};
     string BasicCom = "Basic Computer Engineering";
     string BasicComLab = "Basic Computer Engineering Laboratory";
     string DataCommu = "Data and Computer Communications";
@@ -61,44 +24,28 @@ void Basic_Structure_of_the_Systems(string x){
     string ComNetworksLab = "Computer Networks Laboratory";
     string DatabaseSystems = "Fundamentals of Database Systems";
     string DatabaseSystemsLab = "Database System Laboratory";
-    string Basic_Structure_of_the_Systems_subject[]={"BasicCom","BasicComLab","DataCommu","ComNetworks","ComNetworksLab","DatabaseSystems","DatabaseSystemsLab"};
-    int credit[]={3,2,3,3,1,3,1};
-    int n = sizeof(Basic_Structure_of_the_Systems_key)/sizeof(Basic_Structure_of_the_Systems_key[0]);
-    int count = -1;
-        for(int i = 0; i<n;i++){
-            if(x == Basic_Structure_of_the_Systems_key[i]){
-               count = i;
-                break;
-            }
-        }
 
-        if(count == -1) return 0;
-        else{
-            return Basic_Structure_of_the_Systems_key[count]+" "+Basic_Structure_of_the_Systems_subject[count];
-        }   
-}
-void Hardware_and_Computer_Architecture(string x){
-    //There are 6 subjects in Hardware_and_Computer_Architecture
-    string Hardware_and_Computer_Architecture_key[] = {"261210","261212","261214","261215","261304","261305"};
     string DigitalCircuits = "Logic and Digital Circuits";
     string DigitalCircuitsLab = "Logic and Digital Circuits Laboratory";
     string Microprocessor = "Microprocessor and Interfacing";
     string EmbeddedSystemLab = "Embedded System Laboratory";
     string ComArchitecture = "Computer Architecture";
     string OperatingSystems = "Operating Systems";
-    string Hardware_and_Computer_Architecture_subject[] = {"DigitalCircuits","DigitalCircuitsLab","Microprocessor","EmbeddedSystemLab","ComArchitecture","OperatingSystems"};
-    int credit[]={3,1,3,1,3,3};
-    int n = sizeof(Hardware_and_Computer_Architecture_key)/sizeof(Hardware_and_Computer_Architecture_key[0]);
-    int count = -1;
-        for(int i = 0; i<n;i++){
-            if(x == Hardware_and_Computer_Architecture_key[i]){
-                count = i;
-                break;
+    
+    string Major_subject[] = {"AdvancedComENGR","ProjSurvey","Project","ComProgramming","ObjectProgramming","DataStructres","Algorithms","SoftwareENGR","BasicCom","BasicComLab","DataCommu","ComNetworks","ComNetworksLab","DatabaseSystems","DatabaseSystemsLab","DigitalCircuits","DigitalCircuitsLab","Microprocessor","EmbeddedSystemLab","ComArchitecture","OperatingSystems"};
+    int credit[]={3,1,3,3,3,3,3,3,3,2,3,3,1,3,1,3,1,3,1,3,3};
+    int n = sizeof(Major_key)/sizeof(Major_key[0]);
+        int count = -1;
+            for(int i = 0; i<n;i++){
+                if(x == Major_key[i]){
+                    count = i;
+                    break;
+                }
             }
-        }
 
-        if(count == -1) return 0;
-        else{
-            return Hardware_and_Computer_Architecture_key[count]+" "+Hardware_and_Computer_Architecture_subject[count];
-        }    
+            if(count == -1) return 0;
+            else{
+                return Major_key[count]+" "+Major_subject[count];
+        }  
 }
+
