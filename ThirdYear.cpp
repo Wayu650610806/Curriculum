ebbaa05da5ab5361ca7068d9b479f5d657d2b3e1
KnackPr <peerawich_r@cmu.ac.th>
2023-02-19 12:26:30 +0700

    ปี3เว้ย

--- ThirdYear.cpp
+++ ThirdYear.cpp
@@ -15,7 +15,6 @@ int main(){
     for(int i=0;i<sizeof(T1)/sizeof(T1[0]);i++){
         cout<<"\t\t"<<T1[i]<<"\n";
     }
-
     string T2[]={Major("261305"),CoreCourses("261306"),Major("261361")};
     cout<<"\t\t\t\t Third Year Semester 2\n";
     for(int i=0;i<sizeof(T2)/sizeof(T2[0]);i++){
