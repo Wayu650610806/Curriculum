0e7bd3c75182a521731b7b16a3b1f0f0b935e957
wayu <i@gmail.com>
2023-02-19 15:06:27 +0700

    Cirriculum

--- FirstYear.cpp
+++ FirstYear.cpp
@@ -2,12 +2,10 @@
 #include<string>
 
 using namespace std;
-  
-void ShowCu(int input){
+
+int main(){
     string F1[]={Learner("001101"),ActiveCitizen("140104"),CoreCourses("206161"),CoreCourses("207105"),CoreCourses("207115"),CoreCourses("259104"),CoreCourses("259106"),ActiveCitizen("259191"),Major("261103")};
     string F2[]={Learner("001102"),CoreCourses("206162"),CoreCourses("207106"),CoreCourses("207116"),CoreCourses("252281"),Major("261102")};
-    }
-int SecondYear(){
     string S1[]={Learner("001201"),CoreCourses("206261"),Major("261207"),Major("261210"),Major("261212"),CoreCourses("261216"),Major("261217")};
     cout<<"\t\t\t\t Second Year Semester 1\n";
     for(int i=0;i<sizeof(S1)/sizeof(S1[0]);i++){
@@ -19,9 +17,7 @@ int SecondYear(){
     for(int i=0;i<sizeof(S2)/sizeof(S2[0]);i++){
     cout<<"\t\t"<<S1[i]<<"\n";
     }
-    return 0;
-}
-int ThirdYear(){
+
     string T1[]={Major("261304"),Major("261335"),Major("261336"),Major("261342"),Major("261343")};
     cout<<"\t\t\t\t Third Year Semester 1\n";
     for(int i=0;i<sizeof(T1)/sizeof(T1[0]);i++){
@@ -32,9 +28,6 @@ int ThirdYear(){
     for(int i=0;i<sizeof(T2)/sizeof(T2[0]);i++){
         cout<<"\t\t"<<T2[i]<<"\n";
     }
-    return 0;
-}
-int FourthYear(){
     string Fo1[]={Major("261491")};
     cout<<"\t\t\t\t Four Year Semester 1\n";
     for(int i=0;i<sizeof(Fo1)/sizeof(Fo1[0]);i++){
@@ -45,5 +38,5 @@ int FourthYear(){
     for(int i=0;i<sizeof(Fo2)/sizeof(Fo2[0]);i++){
         cout<<"\t\t"<<Fo2[i]<<"\n";
     }
-    return 0;
+    
 }
