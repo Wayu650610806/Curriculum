1675f681bd70a6eb49d94d626d9e6a6e611c2229
GOLF2546 <Pattarapolnanakorn@gmail.com>
2023-02-19 13:53:50 +0700

    StudyMap GOLF

---
+++ StudyMap.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int StudyMap()
+{   
+    cout << "\tFirst year"<<"\t      |      \tSecondary year"<<"\t      |      \tThird year"<<"\t      |      \tFourth year"<< endl;
+    cout << "1st semester \t2nd semester  |  " <<"1st semester \t2nd semester  |  "<< "1st semester \t2nd semester  |  " << "1st semester \t2nd semester\t" << endl;
+    cout << " 21 credits \t 19 credits   |  " << " 21 credits \t 19 credits   |  " << " 17 credits \t 20 credits   |  " << " 6 credits \t 16 credits\t" << endl;
+    cout << "          \t              |" << "          \t              |" << "          \t              |"<<  endl;
+    cout << "   206161 \t   206162     |" << "    206261 \t   261208     |" << "          \t   261306     |" << "    206161" <<  endl;
+    cout << "  Calculus1 ----> Calculus2 --|>" << " Calculus3 ---->  Numerical   |\t" << "          \t Prob&Stats   |" << " Coop Education" <<  endl;
+    cout << "          \t              |" << "      |________________________|_____________________^ "<< "        |" << endl;
+    cout << "   259191 \t              |" << "    261216 \t   261218     |" << "    261342 \t              |" << "         \t   259192" <<  endl;
+    cout << "  Activity1 \t              |" << " Discrete Math --> Algorithms -|->" << " Database \t              |" << "         \t  Activity2" <<  endl;
+    cout << "          \t              |" << "            _____________^     |" << "      |   \t              |"<<  endl;
+    cout << "   259106 \t              |" << "    261217_|\t              |" << "    261343 \t   261491     |" << "         \t   261492" <<  endl;
+    cout << "  Workshop \t              |" << " Data Structure                | " << "Database Lab \tProject Survey|" << "------------------> Project" <<  endl;
+    cout << "          \t           ___|" << "______^    \t              |" << "          \t              |"<<  endl;
+    cout << "          \t   261102_|   |" << "            \t   261200     |" << "           \t   261361     |" << "         \t   261405" <<  endl;
+    cout << "           \t  Comp Prog --|" << "------------------> OO Prog ---| " << "------------>\tSoftware Engr |" << "----------------> Advance CPE" <<  endl;
+    cout << "          \t              |" << "          \t              |" << "          \t              |"<<  endl;
+    cout << "   261103 \t              |" << "    261207  \t              |" << "           \t              |" << "         \t         " <<  endl;
+    cout << "  Basic CPE ------------------|" << "-->CPE Lab                     | " << "Major Elective \tMajor Elective|" << "         \tMajor Elective" <<  endl;
+    cout << "          \t              |" << "          \t              |" << "          \t              |"<<  endl;
+    cout << "          \t              |" << "            \t   261332     |" << "    261335 \t              |" << "         \t         " <<  endl;
+    cout << "\t\t\t      |" << "   \t\t  Data Comm   | " << "  Networks    \tMajor Elective|" << "         \t" <<  endl;
+    cout << "          \t              |" << "         ______________^       |" << "      |   \t              |"<<  endl;
+    cout << "          \t              |" << "        |  \t              |" << "    261336 \t              |" << "         \t         " <<  endl;
+    cout << "\t\t\t______|" << "________|                      |" << " Networks Lab \t\t      |" << "         \t Free Elective" <<  endl;
+    cout << "          \t       |      |" << "                               |" << "          \t              |"<<  endl;
+    cout << "   207105 \t   207106     |" << "    261210 \t   261214     |" << "   261304 \t   261305     |" << "          \t" <<  endl;
+    cout << "  Physics 1 --->  Physics 2 --|" << "->  Logic -->  Microprocessors-|-" << ">Comp Arch ------->  OS       |" << "         \t Free Elective" <<  endl;
+    cout << "          \t              |" << "       |       _____^  |       |" << "          \t              |"<<  endl;
+    cout << "   207115 \t   207116     |" << "    261212    |   261215       |" << "          \t              |" << "  B.Eng. in Computer Engineering" <<  endl;
+    cout << "Physics Lab1 --> Physics Lab2 |" << "->Logic Lab   |   Embedded Lab |" << "\t\t\t\t      |" << "   2020 Curriculum Study program" <<  endl;
+    cout << "          \t              |" << "              |                |" << "          \t              |"<< "\t\t(cooperative plan)"<<  endl;
+    cout << "   259104 \t   252281     |" << "              |                |" << "          \t              |" << "   major course    math  and sci" <<  endl;
+    cout << "   Drawing\t  Circuits____|" << "______________|                |" << "          \t              |" << "     gen engr\t     gen edu" <<  endl;
+    cout << "          \t              |" << "          \t              |" << "          \t              |"<<  endl;
+    cout << "   001101 \t   001102     |" << "    001201 \t   001225     |" << "          \t              |" << "              others      " <<  endl;
+    cout << " English 1 ----> English 2 ---|>" << " English R/W    English in Sci|\t" << "          \t              |" << " \t\t\t GE" <<  endl;
+    cout << "          \t     |________|" << "________________________^      |" << "          \t              |"<<  endl;
+    cout << "   140104 \t              |" << "          \t              |" << "          \t              |" << "                          " <<  endl;
+    cout << " Citizenship \t      GE      |" << "    GE                         |\t" << "          \t              |" << " Co-creator GE \t Digital Lit GE" <<  endl;
+    return 0;
+}
