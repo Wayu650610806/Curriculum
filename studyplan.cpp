f232270e16f12b3c63d0cbb83740ad6f6da50f8e
wayu <i@gmail.com>
2023-02-19 11:54:02 +0700

    okok

--- studyplan.cpp
+++ studyplan.cpp
@@ -13,7 +13,7 @@ int main(){
     string F1[]={Learner("001101"),ActiveCitizen("140104"),CoreCourses("206161"),CoreCourses("207105"),CoreCourses("207115"),CoreCourses("259104"),CoreCourses("259106"),ActiveCitizen("259191")};
     cout<<"\t\t\t\t First Year Semester 1\n";
     for(int i=0;i<sizeof(F1)/sizeof(F1[0]);i++){
-        cout<<F1[i]<<"\n";
+        cout<<"\t\t"<<F1[i]<<"\n";
     }
     return 0;
 }
