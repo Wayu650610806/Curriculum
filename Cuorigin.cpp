37fb274a9f7e73db0286fd2c07a1235ab6585ca1
Wayu Tharai <iarahtuyaw@gmail.com>
2023-02-26 12:39:48 +0700

    f1

--- Cuorigin.cpp
+++ Cuorigin.cpp
@@ -4,9 +4,45 @@
 #include<vector>
 #include<iomanip>
 using namespace std;
-int F1(){
+int main(){
     cout<<"\t\t\t\t\tFirst Year\n";
     cout<<"First Semester\n";
-    cout<<"	001101	Fundamental English 1	\t\t\t\t\t\t3";
-    return sum;\\เครดิตรวม
+    cout<<"	001101	Fundamental English 1\t\t\t\t\t\t\t\t\t3\n";
+    cout<<"	140104	Citizenship\t\t\t\t\t\t\t\t\t\t3\n";
+    cout<<"	206161	Calculus for Engineering 1\t\t\t\t\t\t\t\t3\n";
+    cout<<"\t207105	Physics for Engineering and Agro-Industry Students 1\t\t\t\t\t3\n";
+    cout<<"\t207115	Physics Laboratory for Engineering and Agro-Industry Students 1\t\t\t\t1\n";
+    cout<<"\t259104	Engineering Drawing\t\t\t\t\t\t\t\t\t3\n";
+    cout<<"\t259106	Workshop Technology\t\t\t\t\t\t\t\t\t3\n";
+    cout<<"\t259191	Principle of Being Professional\t\t\t\t\t\t\t\t1\n";
+    cout<<"\t261103	Basic Computer Engineering\t\t\t\t\t\t\t\t3\n";
+    cout<<"\t\t\t\t\t\t\t\t\t\t\t\ttotal\t21\n";
+
+    cout<<"\t\t\t\t\tFirst Year\n";
+    cout<<"Second Semester\n";
+    cout<<"	001102	Fundamental English 2\t\t\t\t\t\t\t\t\t3\n";
+    cout<<"	206162  Calculus for Engineering 2\t\t\t\t\t\t\t\t3\n";
+    cout<<"\t207106	Physics for Engineering and Agro-Industry Students 2\t\t\t\t\t3\n";
+    cout<<"\t207116	Physics Laboratory for Engineering and Agro-Industry Students 2\t\t\t\t1\n";
+
+    cout<<"\t252281	Fundamentals of Electronic Circuits for Information Systems and Network Engineering\t3\n";
+    cout<<"\t261102	Computer Programming\t\t\t\t\t\t\t\t\t3\n";
+    cout<<"\t\t\t\t\t\t\t\t\t\t\t\ttotal\t19\n";
+    
+    
+    //return 21;
+}
+int F1(){
+    cout<<"\t\t\t\t\tFirst Year\n";
+    cout<<"Second Semester\n";
+    cout<<"	001102	Fundamental English 2\t\t\t\t\t\t\t3\n";
+    cout<<"	206162  Calculus for Engineering 2\t\t\t\t\t\t3\n";
+    cout<<"\t207106	Physics for Engineering and Agro-Industry Students 2\t\t\t3\n";
+    cout<<"\t207116	Physics Laboratory for Engineering and Agro-Industry Students 2\t\t1\n";
+
+    cout<<"\t252281	Fundamentals of Electronic Circuits for Information Systems and Network Engineering\t\t\t\t\t\t3\n";
+    cout<<"\t261102	Computer Programming\t\t\t\t\t\t3\n";
+    cout<<"\t\t\t\t\t\t\t\t\t\ttotal\t19\n";
+    
+    //return 19;
 }
