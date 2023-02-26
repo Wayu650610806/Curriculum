e8b1e40ed4de080ccef0d3eb63d04e677eabd5a8
Wayu Tharai <iarahtuyaw@gmail.com>
2023-02-26 12:05:04 +0700

    vector

---
+++ Cuorigin.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<vector>
+#include<iomanip>
+using namespace std;
+int main(){
+    cout<<"\t\t\t\t\t\t\t\tFirst Year\n";
+    cout<<"First Semester\n";
+    cout<<"	001101	Fundamental English 1	3";
+    
+
+}
