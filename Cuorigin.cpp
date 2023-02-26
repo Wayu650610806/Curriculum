431b2ad007d90988f37ede777569b162257b6590
Wayu Tharai <iarahtuyaw@gmail.com>
2023-02-26 13:25:11 +0700

    รวม

--- Cuorigin.cpp
+++ Cuorigin.cpp
@@ -4,7 +4,7 @@
 #include<vector>
 #include<iomanip>
 using namespace std;
-int main(){
+int F1(){
     cout<<"\t\t\t\t\tFirst Year\n";
     cout<<"First Semester\n";
     cout<<"	001101	Fundamental English 1\t\t\t\t\t\t\t\t\t3\n";
@@ -30,7 +30,7 @@ int main(){
     cout<<"\t\t\t\t\t\t\t\t\t\t\t\ttotal\t19\n";
     
     
-    //return 21;
+    return 21;
 }
 int F1(){
     cout<<"\t\t\t\t\tFirst Year\n";
@@ -44,5 +44,49 @@ int F1(){
     cout<<"\t261102	Computer Programming\t\t\t\t\t\t3\n";
     cout<<"\t\t\t\t\t\t\t\t\t\ttotal\t19\n";
     
-    //return 19;
+    return 19;
+}
+int s11(){
+    cout<<"\t\t\t\t\tSecond Year\n";
+    cout<<"First Semester\n";
+    cout<<"	001201	Critical Reading and Effective Writing	            3\n";
+    cout<<"	206261	Calculus for Engineering 3	                    3\n";
+    cout<<"	261207  Basic Computer Engineering Laboratory	            2\n";
+    cout<<"	261210	Logic and Digital Circuits	                    3\n";
+    cout<<"	261212	Logic and Digital Circuits Laboratory	            1\n";
+    cout<<"	261216	Discrete Mathematics for Computer Engineers	    3\n";
+    cout<<"	261217	Data Structures for Computer Engineers              3\n";
+    cout<<"		    General Education Elective Course               3\n";
+}
+int s12(){
+    cout<<"\t\t\t\t\tSecond Year\n";
+    cout<<"Second Semester\n";
+    cout<<"	001225	English in Science and Technology Context	    3\n";
+    cout<<"	261200	Object-Oriented Programming	                    3\n";
+    cout<<"	261208  Numerical Computation for Engineers	            3\n";
+    cout<<"	261214	Microprocessor and Interfacing	                    3\n";
+    cout<<"	261215	Embedded System Laboratory	                    1\n";
+    cout<<"	261218	Algorithms for Computer Engineers	            3\n";
+    cout<<"	261332	Data and Computer Communications                    3\n";
+    
+}
+
+void Y4S1(){
+    cout<<"\t\t\t\t\tFourth Year\n";
+    cout<<"First Semester\n";
+    cout<<"\t261491	Project Survey  \t\t\t\t\t 1" << endl;
+    cout<<"\t        Major Elective \t\t\t\t\t\t 9" << endl;
+    cout<<"\t        Free Elective  \t\t\t\t\t\t 3" << endl;
+    cout<<"\t\t\t\t\t\t\t\t Total\t 13"<<endl;
+}
+
+void Y4S2(){
+    cout<<"\t\t\t\t\tFourth Year\n";
+    cout<<"First Semester\n";
+    cout<<"\t259192 Skills for Professionalism and Entrepreneurship	\t 1" << endl;
+    cout<<"\t261405 Advanced Computer Engineering Technology	\t\t 3" << endl;
+    cout<<"\t261492	CPE	492	Project	\t\t\t\t 3" << endl;
+    cout<<"\t        Major Elective \t\t\t\t\t\t 9" << endl;
+    cout<<"\t        Free Elective  \t\t\t\t\t\t 3" << endl;
+    cout<<"\t\t\t\t\t\t\t\t Total\t 13"<<endl;
 }
