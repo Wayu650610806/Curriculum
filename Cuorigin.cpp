2a1db7d375675db52c470b236955929ace6012d2
KnackPr <peerawich_r@cmu.ac.th>
2023-02-28 19:23:39 +0700

    เอาF2 ออกจาก F1

--- Cuorigin.cpp
+++ Cuorigin.cpp
@@ -18,18 +18,6 @@ int F1(){
     cout<<"\t261103	Basic Computer Engineering\t\t\t\t\t\t\t\t3\n";
     cout<<"\t\t\t\t\t\t\t\t\t\t\t\ttotal\t21\n";
 
-    cout<<"\t\t\t\t\tFirst Year\n";
-    cout<<"Second Semester\n";
-    cout<<"	001102	Fundamental English 2\t\t\t\t\t\t\t\t\t3\n";
-    cout<<"	206162  Calculus for Engineering 2\t\t\t\t\t\t\t\t3\n";
-    cout<<"\t207106	Physics for Engineering and Agro-Industry Students 2\t\t\t\t\t3\n";
-    cout<<"\t207116	Physics Laboratory for Engineering and Agro-Industry Students 2\t\t\t\t1\n";
-
-    cout<<"\t252281	Fundamentals of Electronic Circuits for Information Systems and Network Engineering\t3\n";
-    cout<<"\t261102	Computer Programming\t\t\t\t\t\t\t\t\t3\n";
-    cout<<"\t\t\t\t\t\t\t\t\t\t\t\ttotal\t19\n";
-    
-    
     return 21;
 }
 int F2(){
