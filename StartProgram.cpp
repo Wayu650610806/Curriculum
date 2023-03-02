5d81d610ac741ca783e4f42260ef60763da001eb
fluk3G123 <nitipong4434@gmail.com>
2023-03-03 02:44:28 +0700

    เพิ่มฟังก์ชันLoop.oStartProgram |Fluk0.0|

--- StartProgram.cpp
+++ StartProgram.cpp
@@ -6,7 +6,9 @@ using namespace std;
 int Start_program(){
     int command=0;
     int y,s;
+    char again;
     cout<<"\t\t\t\t\t\tCurriculum Management\n";
+    do{
     cout<<"Please select\n";
     cout<<"\t1.Show Map\n\t2.Curriculum\n\t3.Edit\n";
     cout<<"Input number : ";
@@ -22,5 +24,9 @@ int Start_program(){
     if(command==3){
         return 1;
     }
+    cout << "\nWant to select a function again? \x1b[1mY\x1b[0m/\x1b[1mN\x1b[0m : ";
+    cin >> again;
+    }while (toupper(again) == 'Y');
+    cout << "\nThank you. Happy to serve you.:)";
     return 0;
 }
