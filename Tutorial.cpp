74ac55ca97d3c1b72aaec0edc0347f9db07dedad
Waphyoo <110945410+Waphyoo@users.noreply.github.com>
2023-03-05 12:31:04 +0700

    okok

---
+++ Tutorial.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+int Tutorial(){
+cout<<"//first you press number which is the topic you want.\n\n";
+
+cout<<"Please select\n";
+cout<<"       1.Tutorial\n";
+cout<<"       2.Show Map\n";
+cout<<"       3.Curriculum\n";
+cout<<"       4.Edit\n";
+cout<<"Input number :\n\n";
+cout<<"-------------------------------------------\n";
+cout<<"Input number : 1 is Tutorial\n";
+cout<<"Tutorial teach you how to use this program.\n";
+cout<<"-------------------------------------------\n";
+cout<<"-------------------------------------------\n";
+cout<<"Input number : 2 is Show Map\n";
+cout<<"Show Map is infographic that show the subject continue others subject.\n";
+cout<<"-------------------------------------------\n";
+cout<<"-------------------------------------------\n";
+cout<<"Input number : 3 is Curriculum\n";
+cout<<"Curriculum show the subject by the semester.\n";
+cout<<"You can open next page by press next.\n";
+cout<<"You can open prev page by press prev.\n";
+cout<<"-------------------------------------------\n";
+cout<<"-------------------------------------------\n";
+cout<<"Input number : 4 is Edit\n";
+cout<<"In Edit you can\n";
+cout<<"-------------------------------------------\n";
+
+return 0;
+
+}
