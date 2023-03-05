673d447b4cff7cf268d9cfafc83e26ba275fae17
GOLF2546 <Pattarapolnanakorn@gmail.com>
2023-03-05 11:41:29 +0700

    05/03/23 11.41

--- Readfile.cpp
+++ Readfile.cpp
@@ -10,121 +10,137 @@ struct course{
 	char code[100];
 	int credit;
 };
-int printreport(vector<course> F1){
+int printreport(vector<course> F1,vector<course> F2,vector<course> S1,vector<course>S2,vector<course> T1,vector<course> T2,vector<course> Fo1,vector<course> Fo2){
 	int credit = 0;
-	for(unsigned int i = 0;i < F1.size(); i++){
+	cout<<"\t\t\t\t" << "First year 1st Semester" << endl;
+	for(unsigned int i = 0;i < F1.size()-1; i++){
 		cout<<F1[i].credit<<" "<< F1[i].name << " ("<< F1[i].code << ")"<<"\n";
 		credit = credit + F1[i].credit;
     }
+	cout<<"\t\t\t\t" << "First year 2nd Semester" << endl;
+	for(unsigned int i = 0;i < F2.size()-1; i++){
+		cout<<F2[i].credit<<" "<< F2[i].name << " ("<< F2[i].code << ")"<<"\n";
+		credit = credit + F2[i].credit;
+    }
+	cout<<"\t\t\t\t" << "Second year 1st Semester" << endl;
+	for(unsigned int i = 0;i < S1.size()-1; i++){
+		cout<<S1[i].credit<<" "<< S1[i].name << " ("<< S1[i].code << ")"<<"\n";
+		credit = credit + S1[i].credit;
+    }
+	cout<<"\t\t\t\t" << "Second year 2nd Semester" << endl;
+	for(unsigned int i = 0;i < S2.size()-1; i++){
+		cout<<S2[i].credit<<" "<< S2[i].name << " ("<< S2[i].code << ")"<<"\n";
+		credit = credit + S2[i].credit;
+    }
+	cout<<"\t\t\t\t" << "Third year 1st Semester" << endl;
+	for(unsigned int i = 0;i < T1.size()-1; i++){
+		cout<<T1[i].credit<<" "<< T1[i].name << " ("<< T1[i].code << ")"<<"\n";
+		credit = credit + T1[i].credit;
+    }
+	cout<<"\t\t\t\t" << "Third year 2nd Semester" << endl;
+	for(unsigned int i = 0;i < T2.size()-1; i++){
+		cout<<T2[i].credit<<" "<< T2[i].name << " ("<< T2[i].code << ")"<<"\n";
+		credit = credit + T2[i].credit;
+    }
+	cout<<"\t\t\t\t" << "Forth year 1st Semester" << endl;
+	for(unsigned int i = 0;i < Fo1.size()-1; i++){
+		cout<<Fo1[i].credit<<" "<< Fo1[i].name << " ("<< Fo1[i].code << ")"<<"\n";
+		credit = credit + Fo1[i].credit;
+    }
+	cout<<"\t\t\t\t" << "Forth year 2nd Semester" << endl;
+	for(unsigned int i = 0;i < Fo2.size(); i++){
+		cout<<Fo2[i].credit<<" "<< Fo2[i].name << " ("<< Fo2[i].code << ")"<<"\n";
+		credit = credit + Fo2[i].credit;
+    }
 	return credit;
 }
-void readfile(vector<course> &F1){
+void readfile(vector<course> &F1,vector<course> &F2,vector<course> &S1,vector<course> &S2,vector<course> &T1,vector<course> &T2,vector<course> &Fo1,vector<course> &Fo2){
     ifstream file("cuorigin.txt");
 	string textline;
-	int y=0,s=0,state=0;
-	cout << "Input Year and Semester : ";
-	cin >> y >> s;
-    // if(y==1 && s==1){
-	// 	while(getline(file,textline)){
-	// 		if(textline=="F2") break;
-	// 		course s; 
-
-	// 		char format[]="%[^,],%[^,],%d";
-	// 		sscanf(textline.c_str(),format,s.code,s.name,&s.credit);
-	// 		//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-			
-			
-	// 		F1.push_back(s); 	
-			
-	// 	}
-	// }
+	int y = 0 ,s = 0,state = 1;
 	int i = 0;
-	if(y==1 && s==1){
-		state = 1;
-	}else if(y==1 && s==2){
-		state = 2;
-	}else if(y==2 && s==1){
-		state = 3;
-	}else if(y==2 && s==2){
-		state = 4;
-	}else if(y==3 && s==1){
-		state = 5;
-	}else if(y==3 && s==2){
-		state = 6;
-	}else if(y==4 && s==1){
-		state = 7;
-	}else if(y==4 && s==2){
-		state = 8;
-	}
-	cout << y << " " << s << " " << state << endl;
 	while(getline(file,textline)){
 		if(state == 1){
-			if(textline == "F2") break;
+			if(textline == "F2"){ 
+				state =2;
+				}
 			course s;
 			char format[]="%[^,],%[^,],%d";
 			sscanf(textline.c_str(),format,s.code,s.name,&s.credit);
 			//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-			F1.push_back(s); 	
+			F1.push_back(s);
 		}
 		if(state == 2){
-			if(textline == "S1") break;
+			if(textline == "S1"){ 
+				state = 3;
+				}
 			if(textline == "F2") i = 1; 
 			if(textline == "F2" || i != 1) continue;
 			course s;
 			char format[]="%[^,],%[^,],%d";
 			sscanf(textline.c_str(),format,s.code,s.name,&s.credit);
 			//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-			F1.push_back(s); 	
+			F2.push_back(s); 	
 		}
 		if(state == 3){
-			if(textline == "S2") break;
+			if(textline == "S2"){ 
+				state = 4;
+				}
 			if(textline == "S1") i = 1; 
 			if(textline == "S1" || i != 1) continue;
 			course s;
 			char format[]="%[^,],%[^,],%d";
 			sscanf(textline.c_str(),format,s.code,s.name,&s.credit);
 			//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-			F1.push_back(s); 	
+			S1.push_back(s); 	
 		}
 		if(state == 4){
-			if(textline == "T1") break;
+			if(textline == "T1"){ 
+				state = 5;
+			}
 			if(textline == "S2") i = 1; 
 			if(textline == "S2" || i != 1) continue;
 			course s;
 			char format[]="%[^,],%[^,],%d";
 			sscanf(textline.c_str(),format,s.code,s.name,&s.credit);
 			//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-			F1.push_back(s); 	
+			S2.push_back(s); 	
 		}
 		if(state == 5){
-			if(textline == "T2") break;
+			if(textline == "T2"){ 
+				state = 6;
+				}
 			if(textline == "T1") i = 1; 
 			if(textline == "T1" || i != 1) continue;
 			course s;
 			char format[]="%[^,],%[^,],%d";
 			sscanf(textline.c_str(),format,s.code,s.name,&s.credit);
 			//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-			F1.push_back(s); 	
+			T1.push_back(s); 	
 		}
 		if(state == 6){
-			if(textline == "Fo1") break;
+			if(textline == "Fo1"){ 
+				state = 7;
+				}
 			if(textline == "T2") i = 1; 
 			if(textline == "T2" || i != 1) continue;
 			course s;
 			char format[]="%[^,],%[^,],%d";
 			sscanf(textline.c_str(),format,s.code,s.name,&s.credit);
 			//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-			F1.push_back(s); 	
+			T2.push_back(s); 	
 		}
 		if(state == 7){
-			if(textline == "Fo2") break;
+			if(textline == "Fo2"){ 
+				state = 8;
+				}
 			if(textline == "Fo1") i = 1; 
 			if(textline == "Fo1" || i != 1) continue;
 			course s;
 			char format[]="%[^,],%[^,],%d";
 			sscanf(textline.c_str(),format,s.code,s.name,&s.credit);
 			//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-			F1.push_back(s); 	
+			Fo1.push_back(s); 	
 		}
 		if(state == 8){
 			if(textline == "Fo2") i = 1; 
@@ -133,7 +149,7 @@ void readfile(vector<course> &F1){
 			char format[]="%[^,],%[^,],%d";
 			sscanf(textline.c_str(),format,s.code,s.name,&s.credit);
 			//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-			F1.push_back(s); 	
+			Fo2.push_back(s); 	
 		}
 	}
 }
--- StartProgram.cpp
+++ StartProgram.cpp
@@ -1,16 +1,53 @@
+// #include<iostream>
+// #include"StudyMap.cpp"
+// #include"Curriculum.cpp"
+// #include"Edit.cpp"
+// using namespace std;
+// vector<course> F1;
+// int Start_program(){
+//     int command=0;
+//     int y,s;
+//     cout<<"\t\t\t\t\t\tCurriculum Management\n";
+//     cout<<"Please select\n";
+//     cout<<"\t1.Show Map\n\t2.Curriculum\n\t3.Edit\n";
+//     cout<<"Input number : ";
+//     cin>>command;
+//     if(command==1) StudyMap();
+//     if(command==2){
+//         cout<<"Form to input Year Semester" << endl;
+//         cout<<"Input Year and Semester : ";
+//         cin >> y;
+//         cin >> s;
+//         ShowCu(y,s);
+//     }   
+//     if(command==3){
+//         readfile(F1);
+//         int total = printreport(F1);
+//         cout << "Total credit : " << total << endl;
+//     }
+//     return 0;
+// }
 #include<iostream>
 #include"StudyMap.cpp"
 #include"Curriculum.cpp"
-#include"Edit.cpp"
+#include"Readfile.cpp"
 using namespace std;
 
+vector<course> F1;
 int Start_program(){
     int command=0;
     int y,s;
     char again;
     cout<<"\t\t\t\t\t\tCurriculum Management\n";
     do{
-    vector<course> F1;
+    vector<course> F1 = {};
+    vector<course> F2 = {};
+    vector<course> S1 = {};
+    vector<course> S2 = {};
+    vector<course> T1 = {};
+    vector<course> T2 = {};
+    vector<course> Fo1 = {};
+    vector<course> Fo2 = {};
     cout<<"Please select\n";
     cout<<"\t1.Show Map\n\t2.Curriculum\n\t3.Edit\n";
     cout<<"Input number : ";
@@ -24,8 +61,8 @@ int Start_program(){
         ShowCu(y,s);
     }   
      if(command==3){
-        readfile(F1);
-        int total = printreport(F1);
+        readfile(F1,F2,S1,S2,T1,T2,Fo1,Fo2);
+        int total = printreport(F1,F2,S1,S2,T1,T2,Fo1,Fo2);
         cout << "Total credit : " << total << endl;
     }
     cout << "\nWant to select a function again? \x1b[1mY\x1b[0m/\x1b[1mN\x1b[0m : ";
