226e037536920d4ae414c589d0fbd302b14216a2
Waphyoo <110945410+Waphyoo@users.noreply.github.com>
2023-03-15 20:17:25 +0700

    Update Tutorial.cpp

--- Tutorial.cpp
+++ Tutorial.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int Tutorial(){
-cout<<"//first you press number which is the topic you want.\n\n";
+cout<<"//first, you press the number which is the topic you want.\n\n";
 
 cout<<"Please select\n";
 cout<<"       1.Tutorial\n";
@@ -13,21 +13,26 @@ cout<<"       4.Edit\n";
 cout<<"Input number :\n\n";
 cout<<"-------------------------------------------\n";
 cout<<"Input number : 1 is Tutorial\n";
-cout<<"Tutorial teach you how to use this program.\n";
+cout<<"The tutorial teaches you how to use this program.\n";
 cout<<"-------------------------------------------\n";
 cout<<"-------------------------------------------\n";
 cout<<"Input number : 2 is Show Map\n";
-cout<<"Show Map is infographic that show the subject continue others subject.\n";
+cout<<"Show Map is an infographic that shows the continuing subject.\n";
 cout<<"-------------------------------------------\n";
 cout<<"-------------------------------------------\n";
 cout<<"Input number : 3 is Curriculum\n";
+cout<<"After you press 3, You have to choose year and semester.\n";
 cout<<"Curriculum show the subject by the semester.\n";
 cout<<"You can open next page by press next.\n";
 cout<<"You can open prev page by press prev.\n";
 cout<<"-------------------------------------------\n";
 cout<<"-------------------------------------------\n";
 cout<<"Input number : 4 is Edit\n";
-cout<<"In Edit you can\n";
+cout<<"After you press 4, You can press 1 to select the subject by pressing the subject code.\n";
+cout<<"Moreover, If you don't know the subject code you can press 2.\n";
+cout<<"We will show every subject that you can enroll in.\n\n";
+cout<<"After you finish the fourth year second semester.\n";
+cout<<"You can delete the subject by pressing the subject code which you want to.\n";
 cout<<"-------------------------------------------\n";
 
 return 0;
